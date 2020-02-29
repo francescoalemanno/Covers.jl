029c3e419193af70a6d55ee46c90183e56a5b836
Francesco Alemanno <50984334+francescoalemanno@users.noreply.github.com>
2020-02-29 15:09:24 +0100

    initial commit

---
+++ src/Covers.jl
@@ -0,0 +1,42 @@
+module Covers
+    import Base.size
+    import Base.getindex
+    import Base.setindex!
+    using SparseArrays
+    export Cover, twicecovered
+
+    struct Cover{N} <: AbstractArray{Bool,N}
+        size::NTuple{N,Int}
+        cov::Vector{BitArray{1}}
+    end
+
+    Cover(size::Int...) where N =Cover(size,[falses(s) for s in size])
+
+    Base.size(A::Cover) = A.size
+
+
+    @inline function boundscheck(A::Cover,I)
+        if length(I) != length(A.size) || (!all(I .<= A.size)) 
+            throw(BoundsError)
+        end
+    end
+    @inline function Base.getindex(A::Cover, I::Int...) 
+        @boundscheck boundscheck(A,I)
+        any(A.cov[i][I[i]] for i in eachindex(I))
+    end
+    @inline function Base.getindex(A::Cover{N}, I::NTuple{N,Int}) where N
+        @boundscheck boundscheck(A,I)
+        sum(A.cov[i][I[i]] for i in eachindex(I))
+    end
+    @inline function Base.setindex!(A::Cover, v::Bool, i,j) 
+        A.cov[i][j]=v
+    end
+
+    @inline function twicecovered(A::Cover)
+        ret=spzeros(Bool,A.size...)
+        for t in eachindex(A)
+            ret[t]=A[Tuple(t)]>1
+        end
+        ret
+    end
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,9 @@
+using Test
+using Covers
+
+cc=Cover(4,8)
+cc[1,1]=true
+cc[2,2]=true
+
+@test all(cc .^ Bool[1 1 1 1 1 1 1 1; 0 1 0 0 0 0 0 0; 0 1 0 0 0 0 0 0; 0 1 0 0 0 0 0 0])
+
