b97ff905f02524989a0f77898abe52dbd506c00e
Francesco Alemanno <francescoalemanno710@gmail.com>
2020-03-02 12:14:20 +0100

    Added setindex! methods, both scalar and cartesian
    They are based on the principle of minimal covering update.

--- src/Covers.jl
+++ src/Covers.jl
@@ -35,15 +35,30 @@ module Covers
         any(A.cov[i][I[i]] for i in eachindex(I))
     end
 
+    setindex!(A::Cover{1}, v::Bool, i::Int) = _cartesian_setindex!(A, v, i)
+    setindex!(A::Cover{N}, v::Bool, i::Int) where N = _scalar_setindex!(A, v, i)
+    setindex!(A::Cover{N}, v::Bool, I::Int...) where N = _cartesian_setindex!(A, v, I...)
 
-
-    @inline function getindex(A::Cover{N}, I::NTuple{N,Int}) where N
+    @inline function _cartesian_setindex!(A::Cover{N}, v::Bool,I::Int...) where N
         @boundscheck boundscheck(A,I)
-        sum(A.cov[i][I[i]] for i in eachindex(I))
+        state=A[I...]
+        if v && !state
+            idx=findmax(size(A))[2]
+            A(idx,I[idx],v)
+            @goto ret
+        end
+        if !v && state
+            for idx in 1:N
+                A(idx,I[idx],v)
+            end
+            @goto ret
+        end
+        @label ret
+        return v
     end
-
-    @inline function setindex!(A::Cover, args...)
-        error("Undefined operation, the syntax to update a cover is Cover(dimension,index,state=T or F)")
+    @inline function _scalar_setindex!(A::Cover, v::Bool,i::Int)
+        I=CartesianIndices(A)[i]
+        _cartesian_setindex!(A::Cover,v, Tuple(I)...)
     end
 
     function (C::Cover)(dim::Int,i::Int,state::Bool)
@@ -57,4 +72,19 @@ module Covers
         end
         ret
     end
+
+
+    @inline function coveredntimes(A::Cover{N}, I::NTuple{N,Int}) where N
+        @boundscheck boundscheck(A,I)
+        sum(A.cov[i][I[i]] for i in eachindex(I))
+    end
+
+    @inline function ncovered(A::Cover)
+        ret=DefaultArray(0,A.size)
+        for t in eachindex(A)
+            ret[t]=coveredntimes(A, Tuple(t))
+        end
+        ret
+    end
+
 end # module
